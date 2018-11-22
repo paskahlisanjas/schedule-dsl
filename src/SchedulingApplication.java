235d6617dd59a04a458f788280db89f07e69fee6
Paskahlis Anjas <pjaswo1204@gmail.com>
2018-11-22 21:13:38 +0700

    simulate parsing

--- src/SchedulingApplication.java
+++ src/SchedulingApplication.java
@@ -1,17 +1,25 @@
 import dbmanager.DatabaseManager;
+import entity.EntityBuilder;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import parser.SchedulingGrammarLexer;
 import parser.SchedulingGrammarParser;
+import entity.Kelas;
+import entity.Penjadwalan;
+import entity.Ruang;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class SchedulingApplication {
     final static String DATABASE_PATH = "schedule.db";
-    final static String cek = "BUAT KELAS IF7602 DENGAN FASILITAS = AC, MEJA, KURSI;";
+    final static String cek = "BUAT \n" +
+            "    RUANG R7602\n" +
+            "DENGAN \n" +
+            "    KAPASITAS = 50\n" +
+            "    FASILITAS = AC, MEJA, KURSI;";
 
     public static void main(String args[]) {
         SchedulingGrammarLexer lexer = new SchedulingGrammarLexer(new ANTLRInputStream(cek));
@@ -19,16 +27,11 @@ public class SchedulingApplication {
 
         String str;
 
-        str = parser.command().getText();
-        System.out.println(str);
+        SchedulingGrammarParser.EkspresiContext ekspresi = parser.ekspresi();
 
-        SchedulingGrammarParser.EntityContext entity = parser.entity();
-//        System.out.println(entity.KELAS().getText());
-        System.out.println(entity.kode());
+        SchedulingGrammarParser.EntitasContext entitas = ekspresi.entitas();
 
-//        str = parser.entity().kode().getText();
-//        System.out.println(str);
-
-//        System.out.println(parser.);
+        Ruang ruang = (Ruang) EntityBuilder.build(entitas);
+        System.out.println(ruang.toString());
     }
 }
