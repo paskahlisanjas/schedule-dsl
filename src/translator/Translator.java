f3aea273c36775b624fd202d002baf5793a0fa0d
Paskahlis Anjas <pjaswo1204@gmail.com>
2018-11-23 14:25:14 +0700

    create constructor

--- src/translator/Translator.java
+++ src/translator/Translator.java
@@ -1,5 +1,23 @@
 package translator;
 
+import entity.Kelas;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import parser.SchedulingGrammarLexer;
+import parser.SchedulingGrammarParser;
+import parser.SchedulingGrammarParser.*;
+
 public class Translator {
-    
+    private EkspresiContext ekspresi;
+
+    public Translator(String input) {
+        SchedulingGrammarLexer lexer = new SchedulingGrammarLexer(new ANTLRInputStream(input));
+        SchedulingGrammarParser parser = new SchedulingGrammarParser(new CommonTokenStream(lexer));
+
+        ekspresi = parser.ekspresi();
+    }
+
+    public Object runCommand() {
+
+    }
 }
