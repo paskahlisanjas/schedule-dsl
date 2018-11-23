d1d7ac5a4571e8caf062562c7d3ab87698634af3
Rizky Elzandi Barik <rizkyebarik@gmail.com>
2018-11-23 17:31:28 +0700

    edit main

--- src/SchedulingApplication.java
+++ src/SchedulingApplication.java
@@ -41,6 +41,19 @@ public class SchedulingApplication {
         Penjadwalan jadwal = new Penjadwalan();
 //        runConsole();
 //        Bikin Kelas
+        List<String> kebutuhan = new ArrayList<>();
+        kebutuhan.add("AC");
+        kebutuhan.add("PAPANTULIS");
+        kebutuhan.add("PROYEKTOR");
+
+        listRuang.add(new Ruang("7604",50,kebutuhan));
+        listRuang.add(new Ruang("7602",70,kebutuhan));
+        listRuang.add(new Ruang("7603",60,kebutuhan));
+
+        listKelas.add(new Kelas("IF4072",kebutuhan,50,1,8));
+        listKelas.add(new Kelas("IF2110",kebutuhan,60));
+        listKelas.add(new Kelas("IF3110",kebutuhan,60));
+
 //        Bikin Ruang
 //        Jadwalkan
 //        for each Kelas, Assign Kelas ke Ruangan dan Waktu
@@ -49,80 +62,7 @@ public class SchedulingApplication {
 //            - Kapasitas ruangan >= jumlah Mhs
 //            - Kebutuhan harus dipenuhi oleh fasilitas kelas
 //            - Preferensi hari dan jam sudah ditentukan
-        for (Kelas kelas : listKelas) {
-            // Cek Preferensi
-            boolean assigned = false;
-            if (kelas.hariPref != 0) {
-                // cari kelas
-                Ruang ruangDipilih = null;
-                for (Ruang ruang : listRuang) {
-                    if (jadwal.isAvailable(ruang, kelas.hariPref, kelas.jamPref)) {
-                        ruangDipilih = ruang;
-                        break;
-                    }
-                }
-                if (ruangDipilih != null) {
-                    assigned = jadwal.assignKelas(kelas, ruangDipilih, kelas.hariPref, kelas.jamPref);
-                }
-            }
-            if (!assigned) {
-                if (kelas.getTingkat() == 2) {
-                    //Tingkat 2, jam 1 ke atas
-                    int hari = 1;
-                    while (!assigned && hari <= 5) {
-                        int jam = 7;
-                        while (!assigned && jam <= 11) {
-                            if (jadwal.cekTingkatSama(kelas, hari, jam)) {
-                                for (Ruang ruang : listRuang) { //cari kelas
-                                    assigned = jadwal.assignKelas(kelas, ruang, hari, jam);
-                                    if (assigned) {
-                                        break;
-                                    }
-                                }
-                            }
-                            jam++;
-                        }
-                        hari++;
-                    }
-                } else if (kelas.getTingkat() == 3) {
-                    //Tingkat 3, jam 7 ke atas, sebelum jam 12
-                    int hari = 1;
-                    while (!assigned && hari <= 5) {
-                        int jam = 1;
-                        while (!assigned && jam <= 5) {
-                            if (jadwal.cekTingkatSama(kelas, hari, jam)) {
-                                for (Ruang ruang : listRuang) { //cari kelas
-                                    assigned = jadwal.assignKelas(kelas, ruang, hari, jam);
-                                    if (assigned) {
-                                        break;
-                                    }
-                                }
-                            }
-                            jam++;
-                        }
-                        hari++;
-                    }
-                } else {
-                    //Tingkat 4, bebas
-                    int hari = 1;
-                    while (!assigned && hari <= 5) {
-                        int jam = 1;
-                        while (!assigned && jam <= 11) {
-                            if (jadwal.cekTingkatSama(kelas, hari, jam)) {
-                                for (Ruang ruang : listRuang) { //cari kelas
-                                    assigned = jadwal.assignKelas(kelas, ruang, hari, jam);
-                                    if (assigned) {
-                                        break;
-                                    }
-                                }
-                            }
-                            jam++;
-                        }
-                        hari++;
-                    }
-                }
-            }
-
-        }
+        jadwal.jadwalkan(listKelas,listRuang);
+        jadwal.printJadwal();
     }
 }
